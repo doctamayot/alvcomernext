b097b477a8d8eea5d624d6675377cce8255e62ab
Hugo Tamayo <doctamayot@hotmail.com>
2022-09-15 16:29:36 -0500

    150922

--- components/inventory/PartsTable.tsx
+++ components/inventory/PartsTable.tsx
@@ -71,7 +71,7 @@ const columns: GridColDef[] = [
     field: "title",
     headerName: "Titulo",
     width: 250,
-    renderCell: ({ row }: GridValueGetterParams) => {
+    renderCell: ({ row }: any) => {
       return (
         <NextLink href={`/admin/parts/${row.id}`} passHref>
           <Link underline="always">{row.title}</Link>
