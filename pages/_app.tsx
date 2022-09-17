f76801ffdf506f313ff67998d03c3dc3f99c9ec9
Hugo Tamayo <doctamayot@hotmail.com>
2022-09-17 12:58:03 -0500

    150922

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,45 @@
-//Next
+// //Next
+// import type { AppProps } from "next/app";
+// import { SessionProvider } from "next-auth/react";
+// //MUI
+// import { CssBaseline, ThemeProvider } from "@mui/material";
+// //App
+// import "../styles/globals.css";
+// import { lightTheme } from "../themes";
+// import { SWRConfig } from "swr";
+// import { UiProvider } from "../context";
+
+// function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
+//   return (
+//     // <SWRConfig
+//     //   value={{
+//     //     // refreshInterval:500,
+//     //     fetcher: (resource, init) =>
+//     //       fetch(resource, init).then((res) => res.json()),
+//     //   }}
+//     // >
+//     //   <UiProvider>
+//     //     <ThemeProvider theme={lightTheme}>
+//     //       <CssBaseline />
+//     <SessionProvider session={session}>
+//       <Component {...pageProps} />
+//     </SessionProvider>
+//     //     </ThemeProvider>
+//     //   </UiProvider>
+//     // </SWRConfig>
+//   );
+// }
+
+// export default MyApp;
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-//MUI
-import { CssBaseline, ThemeProvider } from "@mui/material";
-//App
-import "../styles/globals.css";
-import { lightTheme } from "../themes";
-import { SWRConfig } from "swr";
-import { UiProvider } from "../context";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
   return (
-    // <SWRConfig
-    //   value={{
-    //     // refreshInterval:500,
-    //     fetcher: (resource, init) =>
-    //       fetch(resource, init).then((res) => res.json()),
-    //   }}
-    // >
-    //   <UiProvider>
-    //     <ThemeProvider theme={lightTheme}>
-    //       <CssBaseline />
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
-    //     </ThemeProvider>
-    //   </UiProvider>
-    // </SWRConfig>
   );
 }
 
