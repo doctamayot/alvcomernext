f240b3ae20ca878e2541cec04f3e5bd0a43f6add
Hugo Tamayo <doctamayot@hotmail.com>
2022-09-17 12:18:02 -0500

    150922

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,91 +1,130 @@
-//Next
-import NextLink from "next/link";
-import { GetServerSideProps, NextPage } from "next";
-import { getProviders, signIn, getSession } from "next-auth/react";
-//MUI
+// //Next
+// import NextLink from "next/link";
+// import { GetServerSideProps, NextPage } from "next";
+// import { getProviders, signIn, getSession } from "next-auth/react";
+// //MUI
+// import {
+//   Box,
+//   Button,
+//   Chip,
+//   Divider,
+//   Grid,
+//   Link,
+//   TextField,
+//   Typography,
+// } from "@mui/material";
+// //App
+// import { AuthLayout } from "../../components/layouts";
+// import { useEffect, useState } from "react";
+
+// interface Props {
+//   google: any;
+// }
+
+// const LoginPage: NextPage<Props> = () => {
+//   const [prov, setProv] = useState<any>({});
+
+//   useEffect(() => {
+//     (async () => {
+//       const providers = await getProviders();
+//       setProv(providers as any);
+//     })();
+//   }, []);
+
+//   return (
+//     <AuthLayout title={"Ingresar"}>
+//       <Box sx={{ width: 350, padding: "10px 20px" }}>
+//         <Grid container spacing={2}>
+//           <Grid item xs={12}>
+//             <Button>
+//               <Typography variant="h1" component="h1">
+//                 Iniciar Sesión con{" "}
+//                 {prov && prov.google && (prov.google.name as any)}
+//               </Typography>
+//             </Button>
+//           </Grid>
+
+//           <Grid item xs={12}>
+//             <Button
+//               type="submit"
+//               color="secondary"
+//               className="circular-btn"
+//               size="large"
+//               fullWidth
+//               onClick={() => signIn(prov.google.id, { callbackUrl: "/" })}
+//             >
+//               Ingresar
+//             </Button>
+//           </Grid>
+//         </Grid>
+//       </Box>
+//     </AuthLayout>
+//   );
+// };
+
+// // You should use getServerSideProps when:
+// // - Only if you need to pre-render a page whose data must be fetched at request time
+
+// // export const getServerSideProps: GetServerSideProps = async ({
+// //   req,
+// //   query,
+// // }) => {
+// //   // const providers = await getProviders(); // your fetch function here
+// //   const session = await getSession({ req });
+
+// //   //const { p = "/" } = query;
+
+// //   if (session) {
+// //     return {
+// //       redirect: {
+// //         destination: "/",
+// //         permanent: false,
+// //       },
+// //     };
+// //   }
+
+// //   return {
+// //     props: {},
+// //   };
+// //};
+
+// export default LoginPage;
+
 import {
-  Box,
-  Button,
-  Chip,
-  Divider,
-  Grid,
-  Link,
-  TextField,
-  Typography,
-} from "@mui/material";
-//App
-import { AuthLayout } from "../../components/layouts";
-import { useEffect, useState } from "react";
+  getProviders,
+  signIn as SignIntoProvider,
+  useSession,
+} from "next-auth/react";
 
-interface Props {
-  google: any;
-}
+function signIn({ providers }: any) {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { data: session, status } = useSession();
 
-const LoginPage: NextPage<Props> = () => {
-  const [prov, setProv] = useState<any>({});
-
-  useEffect(() => {
-    (async () => {
-      const providers = await getProviders();
-      setProv(providers as any);
-    })();
-  }, []);
+  console.log(session);
 
   return (
-    <AuthLayout title={"Ingresar"}>
-      <Box sx={{ width: 350, padding: "10px 20px" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Button>
-              <Typography variant="h1" component="h1">
-                Iniciar Sesión con{" "}
-                {prov && prov.google && (prov.google.name as any)}
-              </Typography>
-            </Button>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Button
-              type="submit"
-              color="secondary"
-              className="circular-btn"
-              size="large"
-              fullWidth
-              onClick={() => signIn(prov.google.id, { callbackUrl: "/" })}
-            >
-              Ingresar
-            </Button>
-          </Grid>
-        </Grid>
-      </Box>
-    </AuthLayout>
+    <>
+      {Object.values(providers).map((provider: any) => (
+        <div key={provider.name}>
+          <button
+            onClick={() => SignIntoProvider(provider.id, { callbackUrl: "/" })}
+          >
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </>
   );
-};
+}
 
-// You should use getServerSideProps when:
-// - Only if you need to pre-render a page whose data must be fetched at request time
+export async function getServerSideProps() {
+  const providers = await getProviders();
 
-// export const getServerSideProps: GetServerSideProps = async ({
-//   req,
-//   query,
-// }) => {
-//   // const providers = await getProviders(); // your fetch function here
-//   const session = await getSession({ req });
+  return {
+    props: {
+      providers,
+    },
+  };
+}
 
-//   //const { p = "/" } = query;
-
-//   if (session) {
-//     return {
-//       redirect: {
-//         destination: "/",
-//         permanent: false,
-//       },
-//     };
-//   }
-
-//   return {
-//     props: {},
-//   };
-//};
-
-export default LoginPage;
+export default signIn;
